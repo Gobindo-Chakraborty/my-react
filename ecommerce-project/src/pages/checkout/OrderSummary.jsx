18033ce2845c4ca08446db86046d9e05bd7697ed
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-04-04 08:20:42 +0600

    Create update delivery option feature

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
 import { formatMoney } from "../../utils/money";
-import { DeliveryOptions } from "./deliveryOptions";
+import { DeliveryOptions } from "./DeliveryOptions";
 
-export function OrderSummary({ cart, deliveryOptions }) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -49,6 +49,7 @@ export function OrderSummary({ cart, deliveryOptions }) {
                 <DeliveryOptions
                   cartItem={cartItem}
                   deliveryOptions={deliveryOptions}
+                  loadCart={loadCart}
                 />
               </div>
             </div>
